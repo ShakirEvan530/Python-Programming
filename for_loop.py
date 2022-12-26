a4c4f7f2442e704e67e0452ab0d537808954b993
ShakirEvan530 <101618267+ShakirEvan530@users.noreply.github.com>
2022-12-26 13:00:17 +0600

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,50 @@
+"""
+n = int(input("Enter the last number:"))
+sum = 0
+for x in range(1,n+1,1):
+    sum = sum + x
+print(sum)
+"""
+"""
+n = int(input("Enter the last number:"))
+sum = 0
+for x in range(2, n+1, 2):
+    sum = sum +x
+    print(sum)
+"""
+
+"""
+fruits = ["Apple", "Banana", "Cherry", "Jackfruit"]
+
+for x in fruits:
+     #print(x)
+    if x == "Banana":
+        break
+    print(x)
+"""
+
+"""
+i = 1
+while(i<=20):
+    print(i)
+    i=i+2
+print("End of the program:")
+"""
+
+"""
+i= 20
+while(i<=500):
+    print(i)
+    i = i+2
+print("End of the Program")
+"""
+
+"""
+i= 1
+while(i<=20):
+    print("Bangladesh")
+    i=i+2
+"""
+ sum = 1
+ while(sum<=20):
+     sum = sum + 2
