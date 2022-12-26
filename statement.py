6a236c6d7b71edb8ccbc31119c835997cd922b38
ShakirEvan530 <101618267+ShakirEvan530@users.noreply.github.com>
2022-12-26 12:02:45 +0600

    Add files via upload

---
+++ statement.py
@@ -0,0 +1,8 @@
+print("What is your age:")
+age = int(input())
+if age > 18:
+    print("Yes you can drive the car")
+elif age ==18 :
+    print("You have to apply for it then we can think of it")
+else:
+    print("You can't drive the car")
