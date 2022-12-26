5c8c6397be4659880f7fc7a17e8c42065abee0b7
ShakirEvan530 <101618267+ShakirEvan530@users.noreply.github.com>
2022-12-26 12:47:28 +0600

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,7 @@
+print("Enter the First number you want to take:")
+A = int(input())
+print("Enter the second number You want to take:")
+B = int(input())
+sum = A + B
+print("The sum of the two numbers is =",sum)
+
